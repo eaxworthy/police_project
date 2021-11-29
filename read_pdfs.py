fec4441049beb69b308eda693e0ace33d5455407
Lizzy <axworthy@sonoma.edu>
2021-11-28 22:03:48 -0800

    Added script to automatically turn pdfs into text files

---
+++ read_pdfs.py
@@ -0,0 +1,38 @@
+import PyPDF2 as pf
+import sys
+import re
+
+def read_pdf():
+
+    #read in pdf
+    in_file = input('Pdf Location: ')
+    reader = pf.PdfFileReader(in_file)
+
+    #get number of pages
+    page_count = reader.numPages
+
+    file_name_approved = False
+    while not file_name_approved:
+        output_file = input('Output file path: ') + '.txt'
+        print('File will be written to '+ output_file)
+        approval = ''
+        while approval not in ['yes', 'no']:
+            approval = input('Is this correct? (yes/no)').lower()
+            if approval == 'yes':
+                file_name_approved = True
+
+    with open(output_file, 'w') as output:
+            for i in range(page_count):
+                page = reader.getPage(i)
+
+                #pdf specific cleaning. change according to needs
+                page = re.sub(r"(\d\d:\d\d)", r"\n\1", page.extractText())
+                newline = page.find('\n')
+                page = page[newline+2:]
+                #page = re.sub("\. \. \.", "...", page)
+                #page = re.sub("\n", "", page)
+
+                output.write(page)
+
+    
+read_pdf()
