2025b652142258e39b30870a522ab5412eb1fc38
eaxworthy <lizzieaxworthy@gmail.com>
2021-12-07 20:43:41 -0800

    Edited log processing files

--- read_pdfs.py
+++ read_pdfs.py
@@ -5,21 +5,14 @@ import re
 def read_pdf():
 
     #read in pdf
-    in_file = input('Pdf Location: ')
-    reader = pf.PdfFileReader(in_file)
+    pdf_folder = "original_logs/"
+    in_file = input('Pdf Name: ')
+    reader = pf.PdfFileReader(pdf_folder+in_file)
 
     #get number of pages
     page_count = reader.numPages
 
-    file_name_approved = False
-    while not file_name_approved:
-        output_file = input('Output file path: ') + '.txt'
-        print('File will be written to '+ output_file)
-        approval = ''
-        while approval not in ['yes', 'no']:
-            approval = input('Is this correct? (yes/no)').lower()
-            if approval == 'yes':
-                file_name_approved = True
+    output_file = "converted_logs/text_files/" + in_file[:-3] + 'txt'
 
     with open(output_file, 'w') as output:
             for i in range(page_count):
@@ -29,13 +22,8 @@ def read_pdf():
                 page = re.sub(r"(\d\d:\d\d)", r"\n\1", page.extractText())
                 page = re.sub(r"\s\s+", " ", page)
                 page = re.sub("\.\s\.", ".", page)
-                if i==0:
-                    newline = page.find('\n')
-                    page = page[newline+2:]
                 
-                
-
-                output.write(page)
+                output.write(page[130:]+'\n')
 
     
 read_pdf()
